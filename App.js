6fe0ea0b9939e4c6078807fd009fc0882c3dce8f
olympia15 <olympiamichelle15@gmail.com>
2025-11-21 21:37:28 -0500

    fixed typo

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ export default function App() {
         onFocus={handleFocus}
       />
 
-      {/* clear Button */}
+      {/* clear button */}
       <Button title="Clear" onPress={handleClear} />
 
       {/* display text */}
